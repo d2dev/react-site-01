242350748c68f33cf17da0566e91e84433c9baab
André <claudeandre972@gmail.com>
2020-08-24 19:57:04 -0400

    increase 404 text font size

--- src/containers/Error.js
+++ src/containers/Error.js
@@ -4,7 +4,7 @@ import React from 'react';
 function Error () {
     return(
         <div>
-            <h1 style={{fontSize:'4rem'}}>404</h1>
+            <h1 style={{fontSize:'10rem'}}>404</h1>
             <h2>Not found</h2>
         </div>
     )
