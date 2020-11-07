aa2d15e023494ee791555e4624be3ffaccc38982
André <claudeandre972@gmail.com>
2020-11-06 21:25:22 -0400

    remove old form tags, and import new contactForm compo (with emailJS)

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -1,30 +1,11 @@
 import React from 'react';
 // import { Component } from "react";
 import { Container, Row, Col } from 'reactstrap';
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+// import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+
+import ContactForm from "../Compo/ContactForm";
 
 class Contact extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {textValue:'', emailValue:''};
-
-        this.handleChangeText = this.handleChangeText.bind(this);
-        this.handleChangeEmail = this.handleChangeEmail.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleChangeEmail(event){
-        this.setState({emailValue: event.target.value});
-    }
-    
-    handleChangeText(event){
-        this.setState({textValue: event.target.value});
-    }
-
-    handleSubmit(event){
-        alert('Submitted by : ' + this.state.emailValue +'\n Message : ' + this.state.textValue);
-        event.preventDefault();
-    }
 
     render(){
         return(
@@ -36,23 +17,9 @@ class Contact extends React.Component {
                             <br/>
                             <br/>
                             <br/>
-                            <Form onSubmit={this.handleSubmit}>
-                                <FormGroup row>   
-                                    <Label for="exampleEmail" sm={3}>Email</Label>
-                                    <Col sm={9}>
-                                        <Input type="email" name="email" id="exampleEmail" placeholder="placeholder" onChange={this.handleChangeEmail}/>
-                                    </Col>
-                                </FormGroup>
-                                <FormGroup row>
-                                    <Label for="text1" sm={3}>Enter some text</Label>
-                                    <Col sm={9}>
-                                        <Input type="text" name="text1" id="text1" placeholder="placeholder" onChange={this.handleChangeText}/>
-                                    </Col>
-                                </FormGroup>
-                                <Button>Submit</Button>
-                                <br/>
-                                <br/>
-                            </Form>
+                            <ContactForm/>
+                            <br/>
+                            <br/>
                         </Col>
                     </Row>
                 </Container>
